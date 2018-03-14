4f4e16ef80c824cab91960ca00e85d0966f804aa
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-03-14 11:15:56 -0400

    Adds XMLHttpRequest that pings the freeCodeCamp weather API
    Retrieves various weather information by pinging https://fcc-weather-api.glitch.me/
    with current latitude and longitude found using geolocation.
    Usage: https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
-    $("#latitude").html("latitude: " + position.coords.latitude);
-    $("#longitude").html("longitude: " + position.coords.longitude);
+    var currentLatitude = position.coords.latitude;
+    var currentLongitude = position.coords.longitude;
+
+    $("#latitude").html("latitude: " + currentLatitude);
+    $("#longitude").html("longitude: " + currentLongitude);
+
+    getWeather(currentLatitude, currentLongitude);
   });
+}
+
+function getWeather(currentLat, currentLong) {
+  var requestURL = 'https://fcc-weather-api.glitch.me/api/current?lon='+ currentLong + '&lat=' + currentLat;
+  var request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    var currentWeather = request.response;
+
+    var currentLocation = currentWeather.name + ", " + currentWeather.sys.country;
+    console.log(currentLocation);
+
+    console.log(currentWeather);
+  }
 }
